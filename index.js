67ea0800b2dded038c10779a579a00c5828effe5
JAGADISH KARE <jagadish.kare@agilepoint.com>
2023-01-30 17:49:39 -0500

    view complete

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { CloudStorage , localstore} from "./store.js";
-
-const todoInput = document.querySelector(".todoInput");
-const addBtn = document.querySelector(".addBtn");
-const todoContainer = document.querySelector(".todoContainer");
-
+import {controller} from "./controller.js"
 export function todoListView () {
     return{
+        prepareTodoPara : function () {
+            const taskPara = document.createElement("p")
+            taskPara.className = "task-para"
+            return taskPara
+        },
         prepareTodoItem : function (taskName) {
             const taskNode = document.createElement("span");
             taskNode.className = "task-span"
@@ -13,18 +13,37 @@ export function todoListView () {
             return taskNode
         },
 
-        prepareEditBtn : function () {
+        prepareEditBtn : function (ediId) {
             const editBtn = document.createElement("button");
             editBtn.innerText = "EDIT";
             editBtn.className = "editBn"
+            editBtn.setAttribute("id" , ediId)
+            editBtn.addEventListener("click" , () => {
+                controller().editEvent(editBtn)
+            })
             return editBtn
         },
 
-        prepareDeleteBtn : function () {
+        prepareDeleteBtn : function (delId) {
             const deleteBtn = document.createElement("button");
-            deleteBtn.innerText = "DELETE";
+            deleteBtn.innerText = "DELETE\n";
             deleteBtn.className = "deleteBn"
+            deleteBtn.setAttribute("id" , delId)
+            deleteBtn.addEventListener("click" , () => {
+                controller().deleteEvent(deleteBtn)
+            })
             return deleteBtn
+        },
+
+        prepareSaveBtn : function (savId , edittext) {
+            const saveBtn = document.createElement("button");
+            saveBtn.innerText = "SAVE";
+            saveBtn.className = "saveBn"
+            saveBtn.setAttribute("id" , savId)
+            saveBtn.addEventListener("click",() => {
+                controller().saveEvent(saveBtn , edittext)
+            })
+            return saveBtn
         },
 
         append : function (parent , child ) {
